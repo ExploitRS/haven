dca005d286353cf1dd0841dbacd9be54c6786bee
John Paul <nil@exploit.rs>
2023-08-11 06:45:17 +0900

    Update front-end

--- frontend/haven/components/interface.tsx
+++ frontend/haven/components/interface.tsx
@@ -2,17 +2,38 @@
 import React from 'react';
 import { Switch } from '@nextui-org/switch'
 
+function toggleStatus (status = {}){
+    // const response = fetch('http://0.0.0.0:8080/api/status/door')
+    // const data = response.json
+    // return data
+    return fetch('http://0.0.0.0:8080/api/status/door', {
+        method: "POST",
+        mode: "cors",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ status: !status })
+    })
+        .then(response => response.json())
+        // .then(data => data.status)
+}
+
 export default function UI() {
     const [checked, setChecked] = React.useState(false)
 
-    const handleChange = () => {
-        setChecked(!checked)
+    const handleChange = async () => {
+        // setChecked(!checked)
+        const response = await toggleStatus(checked)
+        const status = response.status
+        // alert(`Is this your full name: ${response.status}`)
+        setChecked(status)
     }
-    const t = "test"
 
     return (
         <div>
             <Switch
+                isSelected={ checked}
                 onChange={handleChange}
                 value={ checked ? "locked" : "unlocked" }
             />
