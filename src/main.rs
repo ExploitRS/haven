bd1399b86a9eb6b8b92f75515b09d68896f088b1
John Paul <nil@exploit.rs>
2023-08-11 06:45:05 +0900

    Update back-end

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,81 @@
+use std::clone;
+use std::fs;
+use std::cmp;
 use serde::{Deserialize, Serialize};
-use actix_web::{http, get, App, HttpServer, Responder, HttpResponse};
+use actix_web::{http, web, get, post, App, HttpServer, Responder, HttpResponse, Error};
 use actix_cors::Cors;
+use toml;
 
-#[derive(Debug, Serialize, Deserialize)]
+const FILENAME: &str = "door.toml";
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Door {
     status: bool,
 }
 
+impl PartialEq for Door {
+    fn eq(&self, other: &Self) -> bool {
+        self.status == other.status
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Cfg {
+    door: Door,
+}
+
+impl Cfg {
+    fn new() -> Cfg {
+        Cfg {
+            door: Door { status: false },
+        }
+    }
+}
+
+// impl From<&Door> for Cfg {
+//     fn from(door: &Door) -> Self {
+//         Cfg {
+//             door,
+//         }
+//     }
+// }
+
 #[get("/api/status/door")]
 async fn status_door() -> HttpResponse {
-    HttpResponse::Ok().json(Door { status: true })
+    let contents = fs::read_to_string(FILENAME)
+        .expect("Something went wrong reading the file");
+
+    let cfg = toml::from_str::<Cfg>(&contents).unwrap();
+    let door = cfg.door;
+
+    HttpResponse::Ok().json(door)
+    // HttpResponse::Ok().json(Door { status: true })
+}
+
+#[post("api/status/door")]
+async fn update_door_status(body: web::Bytes) -> Result<HttpResponse, Error> {
+    println!("{:?}", body);
+    let new_door = serde_json::from_slice::<Door>(&body)?;
+
+    let contents = fs::read_to_string(FILENAME)
+        .expect("Something went wrong reading the file");
+    let cfg = toml::from_str::<Cfg>(&contents).unwrap();
+    let current_door = cfg.door;
+    let mut new_cfg: Cfg = Cfg{ door: current_door.clone() };
+
+    if current_door != new_door {
+        new_cfg = Cfg { door: new_door };
+        let new_contents = toml::to_string(&new_cfg).unwrap();
+        fs::write(FILENAME, new_contents).expect("Unable to write file");
+    }
+
+    Ok(HttpResponse::Ok().json(&new_cfg.door))
+    // let contents = fs::read_to_string(FILENAME)
+    //     .expect("Something went wrong reading the file");
+
+    // let cfg = toml::from_str::<Cfg>(&contents).unwrap();
+    // let door = cfg.door;
+
 }
 
 #[actix_web::main] // or #[tokio::main]
@@ -27,6 +93,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(cors)
             .service(status_door)
+            .service(update_door_status)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
