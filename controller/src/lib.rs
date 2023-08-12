dc42a112b41465a379dd315358acb8064584619e
John Paul <nil@exploit.rs>
2023-08-13 01:11:53 +0900

    Update controller

--- controller/src/lib.rs
+++ controller/src/lib.rs
@@ -6,19 +6,35 @@ pub fn add(left: usize, right: usize) -> usize {
 
 pub struct HavenCntrlr {
     serial_port: Box<dyn serialport::SerialPort>, 
+    degree: u8,
 }
 
 impl HavenCntrlr {
-    pub fn new() {
+    pub fn new() -> Self {
         let serial_port = serialport::new("/dev/ttyUSB0", 9600)
             .timeout(std::time::Duration::from_millis(10))
             .open()
             .expect("Failed to open serial port");
 
-        let ret = HavenCntrlr {
+        HavenCntrlr {
             serial_port,
-        };
+            degree: 0,
+        }
 
+    }
+
+    pub fn is_locked(self) -> bool {
+        self.degree == 90
+    }
+
+    pub fn lock(mut self) -> Result<(), serialport::Error> {
+        self.serial_port.write(&[90])?;
+        Ok(())
+    }
+
+    pub fn unlock(mut self) -> Result<(), serialport::Error> {
+        self.serial_port.write(&[0])?;
+        Ok(())
     }
 }
 
